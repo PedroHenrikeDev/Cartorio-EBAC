b1b2c62d21e8630622c119c3ab8ec6bc69442bdc
PedroHenrikeDev <pedro.mendes0626@gmail.com>
2025-03-10 13:03:06 -0300

    opção de saída
    Adicionado no sistema a opção de saída para o usuário

--- Cartorio.c
+++ Cartorio.c
@@ -1,22 +1,22 @@
-#include <stdio.h> //biblioteca de comunicação com o usuário
-#include <stdlib.h> //biblioteca de alocação de espaço na memória
-#include <locale.h> //boblioteca de alocação de texto por região
-#include <string.h> //biblioteca responsável por cuidar das strings
+#include <stdio.h> //biblioteca de comunicaï¿½ï¿½o com o usuï¿½rio
+#include <stdlib.h> //biblioteca de alocaï¿½ï¿½o de espaï¿½o na memï¿½ria
+#include <locale.h> //boblioteca de alocaï¿½ï¿½o de texto por regiï¿½o
+#include <string.h> //biblioteca responsï¿½vel por cuidar das strings
 
-int registro() // função responsável por cadastrar os usuários no sistema
+int registro() // funï¿½ï¿½o responsï¿½vel por cadastrar os usuï¿½rios no sistema
 {
-	//inicio de criação das variáveis/strings
+	//inicio de criaï¿½ï¿½o das variï¿½veis/strings
 	char arquivo[40];
 	char cpf[40];
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
-	//final da criação das variáveis
+	//final da criaï¿½ï¿½o das variï¿½veis
 	
-	printf("Digite o CPF a ser cadastrado:"); //coletando informações do usuário
+	printf("Digite o CPF a ser cadastrado:"); //coletando informaï¿½ï¿½es do usuï¿½rio
 	scanf("%s", cpf); //%s refere se a strings
 	
-	strcpy(arquivo, cpf); //responsável por copiar os valores da string
+	strcpy(arquivo, cpf); //responsï¿½vel por copiar os valores da string
 	
 	FILE *file; //cria o arquivo
 	file = fopen(arquivo, "w"); //cria o arquivo e o "w" significa escrever
@@ -68,16 +68,16 @@ int registro() // função responsável por cadastrar os usuários no sistema
 
 
 
-int consultar() //função responsável por consultar usuários
+int consultar() //funï¿½ï¿½o responsï¿½vel por consultar usuï¿½rios
 {
 	setlocale(LC_ALL, "portuguese"); //Definindo a linguagem
 	
-	//inicio a criação de variáveis
+	//inicio a criaï¿½ï¿½o de variï¿½veis
 	char cpf[40];
 	char conteudo[200];
-	//final da criação de variáveis
+	//final da criaï¿½ï¿½o de variï¿½veis
 	
-	printf("Digite o CPF que deseja consultar:"); //coleta o dado necessário para consulta
+	printf("Digite o CPF que deseja consultar:"); //coleta o dado necessï¿½rio para consulta
 	scanf("%s",cpf);
 	
 	FILE *file;
@@ -85,12 +85,12 @@ int consultar() //função responsável por consultar usuários
 
 	if(file == NULL)
 	{
-		printf("lamento, este CPF não foi localizado:\n");
+		printf("lamento, este CPF nï¿½o foi localizado:\n");
 	}
 
-	while(fgets(conteudo, 200, file) != NULL) //enquanto o fgets estiver escrevendo na variavel/sring o conteudo até 200 dos valores dentro do arquivo
+	while(fgets(conteudo, 200, file) != NULL) //enquanto o fgets estiver escrevendo na variavel/sring o conteudo atï¿½ 200 dos valores dentro do arquivo
 	{
-		printf("\nEssas são as informações do usuário:/n");
+		printf("\nEssas sï¿½o as informaï¿½ï¿½es do usuï¿½rio:/n");
 		printf("%s", conteudo);
 		printf("\n\n");
 	}
@@ -104,11 +104,11 @@ int deletar()
 {
 	setlocale(LC_ALL,"portuguese"); //definindo a linguagem
 	
-	//inicio das variáveis
+	//inicio das variï¿½veis
 	char cpf[40];
-	//fim das variáveis
+	//fim das variï¿½veis
 	
-	printf("Digite o CPF do usuário a ser deletado: "); //coleta o dado necessário para deletar
+	printf("Digite o CPF do usuï¿½rio a ser deletado: "); //coleta o dado necessï¿½rio para deletar
 	scanf("%s", cpf);	
 	
 	FILE*file;
@@ -117,15 +117,15 @@ int deletar()
 	
 	if(file == NULL)
 	{
-		printf("lamento, o usuário não foi encontrado no sistema.\n");
+		printf("lamento, o usuï¿½rio nï¿½o foi encontrado no sistema.\n");
 		system("pause");
 	}
 	
 	else
 	{
-	fclose(file); // Fecha o arquivo, pois ele é existente
-    remove(cpf); // Agora você pode remover o arquivo
-    printf("Usuário deletado com sucesso!\n");
+	fclose(file); // Fecha o arquivo, pois ele ï¿½ existente
+    remove(cpf); // Agora vocï¿½ pode remover o arquivo
+    printf("Usuï¿½rio deletado com sucesso!\n");
     system("pause");	
 	}
 	
@@ -135,13 +135,13 @@ int deletar()
 
 int main()
 {
-	int opcao=0; //definindo variáveis
+	int opcao=0; //definindo variï¿½veis
 	int laco=1;
 	
 	for(laco=1; laco=1;)
 	{
 		
-		system("cls"); //responsávei por limpar a tela
+		system("cls"); //responsï¿½vei por limpar a tela
 	
 	
 		setlocale(LC_ALL, "portuguese"); //Definindo a linguagem
@@ -151,16 +151,17 @@ int main()
 		printf("\t1 - Registrar nomes:\n"); 
 		printf("\t2 - Consultar nomes:\n");
 		printf("\t3 - Deletar nomes:\n"); 
+		printf("\t4 - Sair do sistema.\n");
 		printf("opção:"); //fim do menu
 	
-		scanf("%d", &opcao); //armazenando a escolha do usuário
+		scanf("%d", &opcao); //armazenando a escolha do usuï¿½rio
 
 		system("cls");
 		
-		switch(opcao) //inicio da seleção do menu
+		switch(opcao) //inicio da seleï¿½ï¿½o do menu
 		{
 			case 1: 
-			registro(); //chamada de funções
+			registro(); //chamada de funï¿½ï¿½es
 			break;
 			
 			case 2:
@@ -171,12 +172,15 @@ int main()
 			deletar();
 			break;
 			
-		
+			case 4:
+			printf("obrigado por utilizar o sistema! :)\n");
+			return 0;
+			break;
 			
 			
 			default: 
 			printf("Lamento, essa opção não está disponível :( , por favor retorne ao menu\n");
-			system("pause"); //fim da seleção
+			system("pause"); //fim da seleï¿½ï¿½o
 			
 		}
 		
